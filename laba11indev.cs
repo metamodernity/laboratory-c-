580fc409815f1ac7e6c115d83c0b8dcfa334abf8
metamodernity <95966082+metamodernity@users.noreply.github.com>
2022-02-28 14:14:33 +0400

    Create laba11indev.cs

---
+++ laba11indev.cs
@@ -0,0 +1,30 @@
+using System;
+class Programm
+
+{
+    static void Main()
+    {
+        double a1;
+        double hight1;
+        double a2;
+        double a3;
+        double a = 1;
+        double b = 2;
+        double t = 3;
+        double x = 4;
+        double r = 5;
+        double t = 5;
+        double d = 6;
+        double low1;
+        double low2;
+        /*a1 = (2*Math.Sin(Math.Exp(Math.Log10(a * t * t)) + Math.Exp(Math.Log10(b)))/2)*(Math.Cos(Math.Exp(Math.Log10(a*t*t) - Math.Exp(Math.Log(b)))));
+        a2 = 9 + ((Math.Log10(Math.Cos(x)))) *c(Math.Log10(Math.Cos(x))));
+        a3 = a1 / a2;*/
+        hight1 = 2*Math.Sin((Math.Exp(Math.Log10(a * t * t)))) - Math.Cos(Math.Exp(Math.Log10(b)));
+        /*Console.WriteLine($"{b1}");
+        Console.Write($"{a}");*/
+        low1 = 9 + (Math.Log10(Math.Pow((Math.Cos(x)), 2))) + 6 * Math.Log10(Math.Cos(x)) + 6(Math.Pow();
+        
+        Console.WriteLine($"{low1}");
+    }
+}
