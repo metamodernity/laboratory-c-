6ddc95870021944d5619b1cd0dcc5c18fb47332a
metamodernity <95966082+metamodernity@users.noreply.github.com>
2022-03-21 12:57:38 +0400

    Update laba4-1-3inDev.cs

--- laba4-1-3inDev.cs
+++ laba4-1-3inDev.cs
@@ -4,9 +4,9 @@ namespace _4_1_3_
 {
     class Position
     {
-        int Position_One = 90;
-        int Position_Two = 80;
-        int Position_Three = 70;
+        public int Position_One;
+        public int Position_Two;
+       public int Position_Three;
         public void Print()
         {
             Console.WriteLine($"Широта: {Position_One}  Долгота: {Position_Two} Высота: {Position_Three}");
@@ -18,8 +18,16 @@ namespace _4_1_3_
         static void Main(string[] args)
         {
             Position xyz = new Position();
+             xyz.Position_One = 90;
+            xyz.Position_Two = 80;
+            xyz.Position_Three = 70;
             xyz.Print();
 
+            Position xyz2 = new Position();
+            xyz2.Position_One = 70;
+            xyz2.Position_Two = 80;
+            xyz2.Position_Three = 90;
+            xyz2.Print();
         }
     }
 }
